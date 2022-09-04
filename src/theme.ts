ef49a5e43fc998a4c9eb1ee3041f561dcf4ff7de
LucetTin5 <lucettin5@gmail.com>
2022-09-04 10:49:51 +0900

    init

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "#FFF",
+  textColor: "#524FA1",
+  accentColor: "#7554F4",
+};
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#151618",
+  textColor: "#FFF",
+  accentColor: "#7554F4",
+};
